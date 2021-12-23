83d35f80469b4a354106182c14cf2f8f94c5ebc7
panyawat kaedpol <pufferbommy@gmail.com>
2021-12-24 00:15:26 +0700

    add searchModal component

--- src/components/SearchModal.jsx
+++ src/components/SearchModal.jsx
@@ -5,20 +5,22 @@ import { AiOutlineSearch } from 'react-icons/ai'
 
 const SearchModal = ({ showSearchModal, setShowSearchModal }) => {
   return (
-    <div className="bg-black opacity-80 z-50 absolute top-0 left-0 w-screen h-screen">
-      <MdOutlineClose
-        onClick={() => setShowSearchModal(!showSearchModal)}
-        className="cursor-pointer text-white absolute text-3xl right-4 top-4"
-      />
-      <div className="flex fixed mt-20 items-center">
-        <AiOutlineSearch className="text-white relative text-2xl left-5 bottom-1" />
-        <input
-          placeholder="Product Search"
-          className="bg-transparent pb-1 placeholder:text-zinc-600 placeholder:text-sm text-white w-[37rem] pl-6 border-b focus:outline-none"
-          type="text"
+    <>
+      <div className="opacity-80 bg-black absolute top-0 left-0 w-screen h-screen">
+        <MdOutlineClose
+          onClick={() => setShowSearchModal(!showSearchModal)}
+          className="cursor-pointer text-white absolute text-3xl right-4 top-4"
         />
+        <div className="flex fixed mt-[5.5rem] items-center w-full">
+          <AiOutlineSearch className="text-white relative text-2xl left-5 bottom-1" />
+          <input
+            placeholder="Product Search"
+            className="bg-transparent pb-1 placeholder:text-zinc-600 text-white w-[calc(100%-50px)] pl-7 border-b focus:outline-none"
+            type="text"
+          />
+        </div>
       </div>
-    </div>
+    </>
   )
 }
 
