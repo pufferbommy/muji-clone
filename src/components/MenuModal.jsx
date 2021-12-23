09738e542e510acea841c41c2a04aa90b314b70f
panyawat kaedpol <pufferbommy@gmail.com>
2021-12-24 00:17:16 +0700

    when user click the menu It bring you to this component for select the menu that you want to go.

---
+++ src/components/MenuModal.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+// icons
+import { MdOutlineClose } from 'react-icons/md'
+
+const MenuModal = ({ showMenuModal, setShowMenuModal }) => {
+  return (
+    <div className="opacity-80 bg-black absolute top-0 left-0 w-screen h-screen">
+      <span>
+        <MdOutlineClose
+          onClick={() => setShowMenuModal(!showMenuModal)}
+          className="cursor-pointer text-white absolute text-3xl right-4 top-4"
+        />
+      </span>
+      <ul className="text-white mt-10 text-center">
+        <li>Online Store</li>
+        <li>About MUJI</li>
+        <li>News</li>
+        <li>Store Info</li>
+        <li>Contact Us</li>
+        <li>Press</li>
+        <li>Events</li>
+        <li>Web Catalog</li>
+      </ul>
+    </div>
+  )
+}
+
+export default MenuModal
