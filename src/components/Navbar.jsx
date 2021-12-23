1b9ed3a97a05a946e5c4641d3b1e6c221033c16b
panyawat kaedpol <pufferbommy@gmail.com>
2021-12-24 00:16:22 +0700

    add new modal is menuModal when user click the munu and this component will show on the screen.

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,32 +5,47 @@ import { AiOutlineMenu, AiOutlineSearch } from 'react-icons/ai'
 
 // components
 import SearchModal from './SearchModal'
+import MenuModal from './MenuModal'
 
 const Navbar = () => {
+  const [showMenuModal, setShowMenuModal] = useState(false)
   const [showSearchModal, setShowSearchModal] = useState(false)
   return (
-    <nav className="flex mx-6 my-3 z-10 items-end justify-between">
-      <span className="p-2 cursor-pointer text-2xl">
-        <AiOutlineMenu />
-      </span>
-      <img
-        className="w-32 object-contain p-2 cursor-pointer"
-        src="https://www.muji.com/img/logo_muji_b.png"
-        alt="mujiLogo"
-      />
-      <span
-        onClick={() => setShowSearchModal(!showSearchModal)}
-        className="p-2 cursor-pointer text-2xl"
-      >
-        <AiOutlineSearch />
-      </span>
+    <>
+      {/* <-- Modals Start --> */}
+      {showMenuModal && (
+        <MenuModal
+          showMenuModal={showMenuModal}
+          setShowMenuModal={setShowMenuModal}
+        />
+      )}
       {showSearchModal && (
         <SearchModal
           showSearchModal={showSearchModal}
           setShowSearchModal={setShowSearchModal}
         />
       )}
-    </nav>
+      {/* <-- Modals End --> */}
+      <nav className="flex mx-6 my-3 z-10 items-end justify-between">
+        <span
+          onClick={() => setShowMenuModal(!showMenuModal)}
+          className="p-2 cursor-pointer text-2xl"
+        >
+          <AiOutlineMenu />
+        </span>
+        <img
+          className="w-32 object-contain p-2 cursor-pointer"
+          src="https://www.muji.com/img/logo_muji_b.png"
+          alt="mujiLogo"
+        />
+        <span
+          onClick={() => setShowSearchModal(!showSearchModal)}
+          className="p-2 cursor-pointer text-2xl"
+        >
+          <AiOutlineSearch />
+        </span>
+      </nav>
+    </>
   )
 }
 
