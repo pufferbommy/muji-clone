ca5bfcbf187a35bc4c97a9566e819601c3e2093a
panyawat kaedpol <pufferbommy@gmail.com>
2021-12-24 16:01:54 +0700

    add Header component to App

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,15 @@ import React from 'react'
 
 // components
 import Navbar from './components/Navbar'
+import Header from './components/Header'
 
 const App = () => {
   return (
     <>
-      <Navbar />
+      <div className="max-w-5xl mx-8 lg:mx-auto">
+        <Navbar />
+        <Header />
+      </div>
     </>
   )
 }
